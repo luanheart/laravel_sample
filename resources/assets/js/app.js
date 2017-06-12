4b9434a6373be4399bc701a5bffacfe86c8e0d5f
许锦 <xujin@corp-ci.com>
2017-06-12 10:17:19 +0800

    用户系统

---
+++ resources/assets/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by xujin on 2017/6/12.
+ */
+window.$ = window.jQuery = require('jquery');
+require('bootstrap-sass');
+
+$(document).ready(function() {
+
+});
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,9 +10,12 @@
 
 <div class="container">
     <div class="col-md-offset-1 col-md-10">
+        @include('shared.messages')
         @yield('content')
         @include('layouts._footer')
     </div>
 </div>
+
+<script src="/js/app.js"></script>
 </body>
 </html>
